584e510b7d3f09cca0fc07f7e20cec643ba2759b
Muhammad Talha <talha18513@gmail.com>
2025-06-09 06:32:47 +0500

    first

--- docs/javascript/footer.js
+++ docs/javascript/footer.js
@@ -2,7 +2,7 @@ const footer = document.getElementById('footer')
 footer.innerHTML = `
  <footer
         class="w-full grid grid-cols-1 md:grid-cols-3 lg:grid-cols-5 gap-6 md:gap-10 p-5 mt-10 mx-auto max-w-7xl p-5">
-        <div class="md:col-span-2 md:pr-16">
+        <div data-aos="fade-up" class="md:col-span-2 md:pr-16">
             <h2 class="md:text-4xl text-3xl text-zinc-800 font-semibold ">Don’t Miss a Thing</h2>
             <p class="md:mt-5 mt-3 text-zinc-600">Subscribe to our newsletter for exclusive deals and updates.</p>
             <form action="" class="mt-5 flex flex-col items-end gap-3">
@@ -11,7 +11,7 @@ footer.innerHTML = `
                 <button class="cursor-pointer transition-all duration-500 hover:opacity-80">Subscribe</button>
             </form>
         </div>
-        <div class="second">
+        <div data-aos="fade-up" data-aos-delay="100" class="second">
             <h2 class="text-2xl text-zinc-800 font-semibold">Quick Link</h2>
             <ul class="mt-5 space-y-3 text-zinc-700 font-medium">
                 <li><a href="/index.html">Home</a></li>
@@ -20,7 +20,7 @@ footer.innerHTML = `
                 <li><a href="/fleet.html">Fleet</a></li>
             </ul>
         </div>
-        <div class="second">
+        <div data-aos="fade-up" data-aos-delay="200" class="second">
             <h2 class="text-2xl text-zinc-800 font-semibold"> The Cars</h2>
             <ul class="mt-5 space-y-3 text-zinc-700 font-medium">
                 <li><a href="#choose">Features</a></li>
@@ -28,7 +28,7 @@ footer.innerHTML = `
                 <li><a href="#ready">Ready For Trip</a></li>
             </ul>
         </div>
-        <div class="second">
+        <div data-aos="fade-up" data-aos-delay="300" class="second">
             <h2 class="text-2xl text-zinc-800 font-semibold">Social Media</h2>
             <ul class="mt-5 space-y-3 text-zinc-700 font-medium">
                 <li><a href="https://www.facebook.com/" target="_blank">Facebook</a></li>
